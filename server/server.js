e1fdb9f98dc73f15fcf626356d7910cea749f428
Emma Stout <emma.stout01@gmail.com>
2017-11-06 15:33:41 -0600

    added heroku port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var port = 5000;
+var port = process.env.PORT || 5000;
 var rental = require('./routes/rental.router.js')
 var listing = require('./routes/listing.router.js');
 var mongoURI = '';
